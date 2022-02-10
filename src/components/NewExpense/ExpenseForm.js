23b823a352e3c8821d479dae8b6ce9965e7224e9
mahbub500 <mahbubmr500@gmail.com>
2022-02-10 18:58:36 +0600

    UI , Expense item, and Expense compolete

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ExpenseForm.css'
+
+const ExpenseForm = () => {
+    return <form >
+
+
+    </form>;
+}
+
+
+
+export default ExpenseForm;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewExpense from './NewExpense.css'
+
+const NewExpense = () => {
+    return <div className='expense'>
+
+    </div>;
+}
+
+// #endregion
+
+export default NewExpense;
